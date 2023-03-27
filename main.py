63855314f9309987e5f247ffa7bd2f083adb5db7
Wanderson Rossetti <pastarossetti@gmail.com>
2023-03-27 20:37:52 -0300

    recove

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-# import sys
-from sys import exit, platform
+# from sys import platform
+# from sys import exit
 
 
-def hello_world(msg):
-    return msg
+# def hello_world(msg):
+#     return msg
 
 
-def soma(n1, n2):
-    soma = n1 + n2
-    return soma
+# def soma(n1, n2):
+#     soma = n1 + n2
+#     return soma
 
 
-ola = hello_world('Olá mundo')
-print(ola)
-soma = soma(1, 3)
-print(soma)
+# def fala(msg, nome='João'):
+#     return msg, nome
 
 
-# platform = 'A minha '
-print(platform)
+# ola = hello_world('Olá mundo')
+# print(ola)
+# soma = soma(1, 3)
+# print(soma)
+# msg = fala('Ola')
+# print(msg)
 
+# # platform = 'A minha '
+# print(platform)
+# exit()
+
+print('Este módulo se chama', __name__)
