676564272487cc745513331aaa0ab7c8d05d531a
Wanderson Rossetti <pastarossetti@gmail.com>
2023-03-27 21:25:11 -0300

    create

--- teste_1.py
+++ teste_1.py
@@ -1,4 +1,9 @@
-from package.modulo import soma_do_modulo
+# from package.modulo import soma_do_modulo
 
-soma = soma_do_modulo(1, 5)
-print(soma)
+# soma = soma_do_modulo(1, 5)
+# print(soma)
+
+from package.modulo_b import fala_oi
+
+fala = fala_oi()
+print(fala)
