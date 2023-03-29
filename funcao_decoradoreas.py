ae17145e989969ba130fd032da4f942946509a1e
Wanderson Rossetti <pastarossetti@gmail.com>
2023-03-29 14:59:50 -0300

    create funcao & decoradores

--- funcao_decoradoreas.py
+++ funcao_decoradoreas.py
@@ -7,6 +7,7 @@ def criar_funcao(func):
     return interna
 
 
+@criar_funcao
 def inverte_string(string):
     return string[::-1]
 
@@ -16,7 +17,6 @@ def e_string(param):
         raise TypeError('param deve ser uma string')
 
 
-inverte_string_checando_paramentro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_paramentro('123')
+# inverte_string_checando_paramentro = criar_funcao(inverte_string)
+invertida = inverte_string('123')
 print(invertida)
-
