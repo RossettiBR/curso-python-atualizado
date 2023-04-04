3757ded8fb7be5d6f8eb509d95c008600275340d
Wanderson Rossetti <pastarossetti@gmail.com>
2023-04-04 10:26:56 -0300

    create file func itertools

---
+++ combinations_permutations_product.py
@@ -0,0 +1,23 @@
+from itertools import combinations, permutations, product
+
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculina', 'feminina', 'unisex'],
+    ['algodão', 'poliester'],
+]
+
+# print_iter(combinations(pessoas, 2))
+# print_iter(permutations(pessoas, 2))
+
+print_iter(product(*camisetas))
