605cd8a1ef79acb1b69aa2c08918e2255b6676bc
Wanderson Rossetti <pastarossetti@gmail.com>
2023-04-19 09:41:59 -0300

    att funcao recursiva

--- funcao_recursivas.py
+++ funcao_recursivas.py
@@ -1,15 +1,31 @@
-def recursiva(inicio=0, fim=10):
-    # Caso base
-    if inicio >= fim:
-        return fim
+# import sys
 
-    print(inicio, fim)
-
-    # Caso recursivo
-    # contar até chegar no final
-
-    inicio += 1
-    return recursiva(inicio, fim)
+# sys.setrecursionlimit(1004)
 
 
-print(recursiva())
+# def recursiva(inicio=0, fim=10):
+#     # Caso base
+#     if inicio >= fim:
+#         return fim
+
+#     print(inicio, fim)
+
+#     # Caso recursivo
+#     # contar até chegar no final
+
+#     inicio += 1
+#     return recursiva(inicio, fim)
+
+
+# print(recursiva(0, 1001))
+
+
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
