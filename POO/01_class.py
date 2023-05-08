84e3071e1d65c737b59d71fd8abdfa3f9445ec9f
Wanderson Rossetti <pastarossetti@gmail.com>
2023-05-08 10:46:27 -0300

    update

---
+++ POO/01_class.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+        return
+
+
+p1 = Pessoa('Luiz', 'Otavio')
+
+
+print(p1.nome)
+print(p1.sobrenome)
