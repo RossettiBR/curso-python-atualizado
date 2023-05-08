cadabef3a82297a898201b3bcc9025eae6865a5b
Wanderson Rossetti <pastarossetti@gmail.com>
2023-05-08 14:59:29 -0300

    create

---
+++ POO/03_metodo_instancias_de_classes.py
@@ -0,0 +1,14 @@
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando')
+
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro(nome='Celta')
+print(celta.nome)
