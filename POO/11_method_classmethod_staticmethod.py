6ecec4354ee6affa743802f56cc347d88b7db1cf
Wanderson Rossetti <pastarossetti@gmail.com>
2023-05-09 10:21:15 -0300

    create method classmeehod

---
+++ POO/11_method_classmethod_staticmethod.py
@@ -0,0 +1,24 @@
+class Connection:
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+
+    def set_user(self, user):
+        # setter
+        self.user = user
+
+    def set_passsword(self, password):
+        self.password = password
+
+    @classmethod
+    def create_with_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+
+
+c1 = Connection.create_with_auth('Wander', '123')
+print(c1.user)
+print(c1.password)
