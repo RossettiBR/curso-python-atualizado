95f1bbe4574e87d452c5207ea6177e2b5cc787b3
Wanderson Rossetti <pastarossetti@gmail.com>
2023-05-15 10:32:34 -0300

    update create file txt

--- pasta_arquivos_mixin/log.py
+++ pasta_arquivos_mixin/log.py
@@ -1,12 +1,17 @@
 # Abstração
 
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
-    
+
     def log_error(self, msg):
         return self._log(f'Error: {msg}')
-    
+
     def log_success(self, msg):
         return self._log(f'Success: {msg}')
 
@@ -14,6 +19,11 @@ class Log:
 class LogFileMixin(Log):
     def _log(self, msg):
         print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 
 class LogPrintMixin(Log):
