b46616e5376cfd2f267b26d03f301e5c042d032f
Wanderson Rossetti <pastarossetti@gmail.com>
2023-05-17 09:02:08 -0300

    update

--- POO/28_dunder_methods_repr_str.py
+++ POO/28_dunder_methods_repr_str.py
@@ -6,11 +6,11 @@ class Ponto:
 
     def __str__(self):
         return f'{self.x}, {self.y}'
-    
+
     def __repr__(self):
         class_name = type(self).__name__
         return f'{class_name} (x={self.x!r}, y={self.y!r}, z={self.z!r})'
-    
+
 
 p1 = Ponto(1, 2)
 p2 = Ponto(978, 345)
