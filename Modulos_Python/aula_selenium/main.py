723137dc884680ccbb5009ddd69cf1e722e85bfd
Wanderson Rossetti <pastarossetti@gmail.com>
2023-06-07 09:20:41 -0300

    create code selenium

--- Modulos_Python/aula_selenium/main.py
+++ Modulos_Python/aula_selenium/main.py
@@ -1,1 +1,31 @@
-print('Hello World!')
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+ROOT_FOLDER = Path(__file__).parent
+CHROMEDRIVER_EXEC = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
+
+    chrome_service = Service(
+        executable_path=str(CHROMEDRIVER_EXEC)
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+if __name__ == '__main__':
+    options = ()
+    browser = make_chrome_browser(*options)
+    browser.get('https://www.google.com.br')
