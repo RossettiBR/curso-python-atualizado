836c3595959753bab7d501916f655c5d31dd5fda
Wanderson Rossetti <pastarossetti@gmail.com>
2023-06-14 11:09:35 -0300

    create pdfs imgens merge

--- Modulos_Python/27_pdfs/main.py.py
+++ Modulos_Python/27_pdfs/main.py.py
@@ -1,3 +1,33 @@
 from pathlib import Path
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfMerger, PdfWriter
 
+PASTA_RAIZ = Path(__file__).parent
+PASTA_ORIGINAIS = PASTA_RAIZ / 'pdf'
+PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos'
+
+RELATORIO_BACEN = PASTA_ORIGINAIS / 'R20230609.pdf'
+
+PASTA_NOVA.mkdir(exist_ok=True)
+
+reader = PdfReader(RELATORIO_BACEN)
+
+page0 = reader.pages[0]
+imagem0 = page0.images[0]
+
+for i, page in enumerate(reader.pages):
+    writer = PdfWriter()
+    with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
+        writer.add_page(page)
+        writer.write(arquivo)
+
+files = [
+    PASTA_NOVA / 'page1.pdf',
+    PASTA_NOVA / 'page0.pdf',
+]
+
+merger = PdfMerger()
+for file in files:
+    merger.append(file)
+
+merger.write(PASTA_NOVA / 'MERGED.pdf')
+merger.close()
