3ec9647b4ceef5a489c939e60e1f476b78ef71c7
Wanderson Rossetti <pastarossetti@gmail.com>
2023-06-20 09:54:16 -0300

    update

--- pyside6/calculadora/main.py
+++ pyside6/calculadora/main.py
@@ -1,13 +1,13 @@
 import sys
 
+from buttons import Button, ButtonsGrid
 from display import Display
+from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from variables import WINDOW_ICONS
-from info import Info
 from style import setupTheme
-from buttons import Button
+from variables import WINDOW_ICONS
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -20,15 +20,18 @@ if __name__ == '__main__':
 
     # Info
     info = Info('2.0 ^ 10.0 = 1024')
-    window.addToVLayout(info)
+    window.addWidgetToVLayout(info)
 
     # Display
     display = Display()
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
+
+    # Grid
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
 
     # Button
-    button = Button('Texto do botão')
-    window.addToVLayout(button)
+    buttonsGrid.addWidget(Button('0'), 0, 0)
 
     # executa tudo
     window.adjustFixedSize()
