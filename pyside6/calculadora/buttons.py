e428b1f63512efb0512aa840387cc1d391c00e59
Wanderson Rossetti <pastarossetti@gmail.com>
2023-06-26 11:01:45 -0300

    update

--- pyside6/calculadora/buttons.py
+++ pyside6/calculadora/buttons.py
@@ -29,7 +29,7 @@ class ButtonsGrid(QGridLayout):
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
-            ['C', '◀', '^', '/'],
+            ['C', 'D', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
@@ -89,7 +89,7 @@ class ButtonsGrid(QGridLayout):
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
 
-        if text == '◀':
+        if text == 'D':
             self._connectButtonClicked(button, self.display.backspace)
 
         if text in '+-/*':
